a2b724a9c568b9659b90e44796afe69543861d30
Jessmark-11 <116412092+Jessmark-11@users.noreply.github.com>
2022-11-05 21:46:38 +0800

    Update MainProgram.java

--- MainProgram.java
+++ MainProgram.java
@@ -1,1 +1,12 @@
-# OOPExercise2
+public claas MainProgram { // #1 Main class
+    public static void main(String[] args) {
+        Pet pet;    // superclass object
+        Dog dog = new Dog("Brownie", 10, "German Sheperd"); // subclass object
+        Parrot parrot = new Parrot("Tweetie", 1, "Larazzabal");// another subclass object
+        pet = dog;
+        // calling the accessors from the superclass object
+       System.out.println(pet.makeSound() + " " + "My name is " + pet.getName());
+       pet = parrot,
+       System.out.println(pet.makeSound() + " " + "My name is " + pet.getName());
+    }
+}  
