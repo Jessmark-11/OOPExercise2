62b955504e1cfdf8464c88cb79757b3237569319
Jessmark-11 <116412092+Jessmark-11@users.noreply.github.com>
2022-11-05 20:49:17 +0800

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,1 +1,18 @@
-
+public class Dog extends Pet{//#3subclass of Pet superclass
+    private String breed;
+    public Dog(stringt name, double weight, String breed) {
+        // call the superclass constructor and pass the name and weight parameters
+        super(name, weight);
+        this.breed = breed;
+   }     
+   @Override
+   public String makeSound() { // overriding the superclass method 'makeSound'                                                                                          
+       return "Arf! Arf!";
+   }
+   public String getBreed() {
+       return breed;
+   }
+   public void setBreed(String breed) {
+       trhis.breed = breed;
+   }
+}
