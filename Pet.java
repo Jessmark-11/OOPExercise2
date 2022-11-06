b9f75fd74059992066f57133452c7ef35941a31e
Jessmark-11 <116412092+Jessmark-11@users.noreply.github.com>
2022-11-06 11:22:44 +0800

    Update Pet.java

--- Pet.java
+++ Pet.java
@@ -1,1 +1,23 @@
-# OOPExercise2
+public abstract class Pet { // #2 this is our superclass
+   protected String name; // attributes are protected
+   protected double weight;
+   // constructor
+   public Pet(String nmae, double weight) {
+       this.name = name;
+       this.weight = weight;
+   }
+   // this method will be overrriden by its subclass
+   public abstract String makeSound();
+   public String getName() {
+       rewturn name;
+   }
+   public void setName(String name) {
+       this.name = name;
+   }
+   public double getWeight() {
+       return weight;
+   }
+   public void setWeight(double weight) {
+       this.weight = weight;
+   }
+}
