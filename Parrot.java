111cd4bb374fd017a9b4b63f48be3c03b8c85a1d
Jessmark-11 <116412092+Jessmark-11@users.noreply.github.com>
2022-11-06 11:11:17 +0800

    Update Parrot.java

--- Parrot.java
+++ Parrot.java
@@ -1,1 +1,17 @@
-# OOPExercise2
+public claas Parrot extends Pet { // #3 anpther subclass of Pet superclass
+    private String jungle;
+    public Parrot(String name, double weight, String jungle) {
+        // call the superclass constructor and pass the name and weight parameters
+        super(name, weight);
+        this.jungle = jungle;
+    }
+    @override
+    public String makeSound() { // overriding the superclass method 'makeSound'
+        return "Hello!!!";
+    public String getJungle() {
+        return jungle;
+    }
+    public void setJungle(String jungle) {
+        this.jungle = jungle;
+    }
+}  
